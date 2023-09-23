24758e2c9f65449b29fe817272db218243cf4bdb
SHAILJA <145263149+Shailjar21@users.noreply.github.com>
2023-09-22 20:12:01 -0700

    Add files via upload

---
+++ add.py
@@ -0,0 +1,5 @@
+x=int(input('enter a no a'))
+y=int(input('enter a no b'))
+sum=x+y
+print('sum=',sum)
+      
---
+++ perimeter of a triangle.py
@@ -0,0 +1,6 @@
+x=int(input("length of first side"))
+y=int(input("length of second side"))
+z=int(input("length of third side"))
+perimeter=x+y+z
+print("perimeter=",perimeter)
+    
