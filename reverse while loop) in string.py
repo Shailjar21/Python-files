2b0f85a3f8ae693bfeb8bc7450b3705ace037eaf
SHAILJA <145263149+Shailjar21@users.noreply.github.com>
2023-10-13 21:33:42 -0700

    Add files via upload

---
+++ reverse while loop) in string.py
@@ -0,0 +1,7 @@
+name=('Python is a programming language')
+length=len(name)
+a=-1
+while a>-length:
+    print(name[a])
+    a=a-1
+print('reverse string completed')
---
+++ while loop in string.py
@@ -0,0 +1,12 @@
+name=('Python is a programming language')
+length=len(name)
+a=0
+while a<length:
+    print(name[a])
+    a=a+1
+print('foreward count completed')
+a=0
+while a<length:
+    print(name[a])
+    a=a-1
+print('reverse string completed')
